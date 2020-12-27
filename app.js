b0d3e14f54f2cb3e1cbd8690164ae5291435ac00
S. Sembara <sembara9090@gmail.com>
2020-12-27 18:49:45 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const env = require('dotenv').config()
+const bodyParser = require('body-parser')
+const scrapedin = require('scrapedin')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+// config
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// router
+app.use('/api/hello', async (req, res) => {
+    res.send("hello")
+})
+
+app.post('/api/linkedin', async (req, res) => {
+
+    const { linkedin } = req.body
+
+    const profileScraper = await scrapedin({ email: process.env.LINKEDIN_EMAIL, password: process.env.LINKEDIN_PASSWORD })
+    const profile = await profileScraper(linkedin)
+    res.status(200).send({
+        data : profile
+    })
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server is running in ${port}`)
+})
